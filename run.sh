d2ee7a6d774e9b9f601577e7c112935bdd8dd769
Ian Miell <ian.miell@gmail.com>
2016-06-02 10:11:53 +0100

    TODO

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-shutit skeleton --shutitfile Shutitfile
+#!/bin/bash
+set -x
+set -e
+rm -rf /tmp/home_server
+shutit skeleton --shutitfile Shutitfile --module_directory /tmp/home_server --module_name home_server --domain tk.shutit --delivery bash --template_branch bash
