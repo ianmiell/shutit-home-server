04e0a07f740f59b980b070614ff8c98ff43bb56d
Ian Miell <ian.miell@gmail.com>
2016-06-11 18:01:34 +0100

    added in ssh-keygen

--- run.sh
+++ run.sh
@@ -2,4 +2,10 @@
 set -x
 set -e
 rm -rf /tmp/home_server
-shutit skeleton --shutitfile Shutitfile --module_directory /tmp/home_server --module_name home_server --domain tk.shutit --delivery bash --template_branch bash
+shutit skeleton --shutitfile Shutitfile \
+	--module_directory /tmp/home_server \
+	--module_name home_server \
+	--domain tk.shutit \
+	--delivery bash \
+	--template_branch bash
+cd /tmp/home_server && ./run.sh
