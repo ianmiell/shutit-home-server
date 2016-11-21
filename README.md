544fa3828755230893443d45038d62f28fb3e3e1
Ian Miell <ian.miell@gmail.com>
2016-11-21 17:14:01 +0000

    run instructions

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Example of a Shutitfile automation script
 ```
 sudo pip install shutit
 git clone https://github.com/ianmiell/shutit-home-server && cd shutit-home-server
-sudo ./run.sh
+shutit run
 ```
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf /tmp/home_server
-shutit skeleton --shutitfile Shutitfile \
-	--name /tmp/home_server \
-	--domain tk.shutit \
-	--delivery bash
-cd /tmp/home_server && ./run.sh "$@"
