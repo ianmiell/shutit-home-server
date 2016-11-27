6407b0c3252f58aa5ce2b6b50f3f5bdbab915824
Ian Miell <ian.miell@gmail.com>
2016-11-27 19:22:32 +0000

    pip

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@ rm -rf /tmp/home_server
 shutit skeleton --shutitfile Shutitfile \
 	--name /tmp/home_server \
 	--domain tk.shutit \
-	--delivery bash \
-	--template_branch bash
+	--delivery bash
 cd /tmp/home_server && ./run.sh "$@"
